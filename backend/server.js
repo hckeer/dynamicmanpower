b9e3c1549cf4adf47e518fab17da7404caa803d2
hckeer <aayam.neupane1@gmail.com>
2025-05-22 10:08:39 +0545

    Initial commit after cleanussasssssssssssp

--- backend/server.js
+++ backend/server.js
@@ -34,7 +34,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, '../uploads'));
+    cb(null, uploadDir);
   },
   filename: function (req, file, cb) {
     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
